d5ec3c65345d7c1a9a16616fbbfc9e3412c874ea
Raphael-Borges <raps_rnb@hotmail.com>
2023-01-30 12:31:37 -0300

    update whitenoise settings

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -155,3 +155,5 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'  
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
