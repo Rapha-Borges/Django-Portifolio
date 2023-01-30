3838e7a06bbcd9b4a3d5732c1a4e6fea24a6734b
Raphael-Borges <raps_rnb@hotmail.com>
2023-01-29 21:49:21 -0300

    create environments

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
+from decouple import config, Csv
 
-SECRET_KEY = 123456789 #TODO
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
